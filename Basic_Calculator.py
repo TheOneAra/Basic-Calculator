489080b7f6e0304bd144f0c4e6e0ac13f441a311
Ara <46380141+TheOneAra@users.noreply.github.com>
2019-02-12 18:22:28 -0700

    Basic_Calculator.py
    Added the "power" mode ( ____ to the power of ____ ), and made it so the user can now use the calculator multiple times without closing the program first.

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -1,42 +1,66 @@
-  # List of modes to print
-modes = ["Addition(+)", "Subtraction(-)", "Multiplication(*)", "Division(/)"]
+  # List all the modes available to print
+modes = ["1. Addition(+)", "2. Subtraction(-)", "3. Multiplication(*)", "4. Division(/)", "5. Power(^)"]
 
-  # Define a function called 'calc' that calculates the user input
+  # Define a function that asks if the user wants to use the calculator again
+def another():
+    another = input("Solve another equation? (Y/N)\n")
+    while another in "y" "Y":
+        mainCalc()
+        break
+    else:
+        input("Program complete! Press ENTER to exit. ")
+
+  # Define a function called 'calc____' that calculates the user input depending on the mode selected
 def calcAddition(x, y):
     print("{} + {} = {}!".format(x, y, x+y))
-    input("Program complete, press ENTER to exit~. ")
+    another()
 def calcSubtraction(x, y):
     print("{} - {} = {}!".format(x, y, x-y))
-    input("Program complete, press ENTER to exit~. ")
+    another()
 def calcMultiplication(x, y):
     print("{} * {} = {}!".format(x, y, x*y))
-    input("Program complete, press ENTER to exit~. ")
+    another()
 def calcDivision(x, y):
     print("{} ÷ {} = {}!".format(x, y, x/y))
-    input("Program complete, press ENTER to exit~. ")
+    another()
+def calcPower(xpow, ypow):
+    print("{} to the power of {} = {}!".format(xpow, ypow, pow(xpow,ypow)))
+    another()
 
-  # Print the list of modes
-print ("Select mode: ")
-for x in modes:
-    print (x)
+  # Define a function "mainCalc" that holds the main function of the calculator
+def mainCalc():
+  # Print the list of modes    
+    print ("Select mode: ")
+    for x in modes:
+        print (x)
 
-  # Ask the user to select a mode (+, -, *, or /)
-select = input()
+  # Ask the user to select a mode (+, -, *, /, or ^)
+    select = input()
 
-  # Ask the user what numbers they want to calculate
-x = float(input("First number: "))
-y = float(input("Second number: "))
-
-  # If user selects +, -, *, or /, calculate x and y together
-if select in ["+", "Addition", "addition"]:
-    calcAddition(x,y)
-elif select in ["-", "Subtraction", "subtraction"]:
-    calcSubtraction(x,y)
-elif select in ["*", "Multiplication", "multiplication"]:
-    calcMultiplication(x,y)
-elif select in ["/", "Division", "division"]:
-    calcDivision(x,y)
+  # If the user selects one of the modes, ask the user what numbers they want to calculate, then calculate the two numbers
+    if select in ["+", "Addition", "addition", "1"]:
+        x = float(input("First number: "))
+        y = float(input("Second number: "))
+        calcAddition(x,y)
+    elif select in ["-", "Subtraction", "subtraction", "2"]:
+        x = float(input("First number: "))
+        y = float(input("Second number: "))
+        calcSubtraction(x,y)
+    elif select in ["*", "Multiplication", "multiplication", "3"]:
+        x = float(input("First number: "))
+        y = float(input("Second number: "))
+        calcMultiplication(x,y)
+    elif select in ["/", "Division", "division", "4"]:
+        x = float(input("First number: "))
+        y = float(input("Second number: "))
+        calcDivision(x,y)
+    elif select in ["^", "Power", "power", "5"]:
+        xpow = float(input("Number: "))
+        ypow = float(input("Power: "))
+        calcPower(xpow, ypow)
 
   # If something fucks up, show an error message
-else:
-    print("An error has occured. Press enter to exit the program.")
+    else:
+        input("An error has occured. Press ENTER to exit.")
+        
+mainCalc()
